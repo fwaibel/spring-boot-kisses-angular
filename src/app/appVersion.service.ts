dece1ee0d4fd790775365152386d49c89e4080b0
fluffi <fluffi@datenkollektiv.de>
2017-08-05 14:15:30 +0200

    Code formatting only

--- src/app/appVersion.service.ts
+++ src/app/appVersion.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 
 import 'rxjs/add/operator/map';
 import {AppVersion} from "./model/appVersion";
--- src/app/homepage.module.ts
+++ src/app/homepage.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppVersionComponent} from './appVersion.component';
 
